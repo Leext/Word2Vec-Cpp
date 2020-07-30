78f7aa3042f6dc16d5dab771a9b50c906c412729
lee <leext@buaa.edu.cn>
2020-07-30 18:11:59 +0800

    去除了Trie树

--- include/WordDict.h
+++ include/WordDict.h
@@ -10,14 +10,6 @@
 #include <unordered_map>
 #include <memory>
 
-struct TrieNode
-{
-    std::unordered_map<char, int> children;
-    int wordIdx;
-
-    TrieNode();
-};
-
 struct Word
 {
     std::shared_ptr<std::string> word;
@@ -45,22 +37,6 @@ public:
     std::vector<Word> vocab;
 };
 
-class TrieDict : public WordDict
-{
-public:
-    TrieDict();
-
-    void insert(std::string &word) override;
-
-    void insert(std::string &&word) override;
-
-    bool find(std::string &word) override;
-
-private:
-    int rootIdx;
-    std::vector<TrieNode> nodes;
-};
-
 class HashDict : public WordDict
 {
 public:
@@ -76,73 +52,6 @@ private:
 
 };
 
-
-TrieDict::TrieDict()
-{
-    rootIdx = 0;
-    nodes.emplace_back();
-}
-
-void TrieDict::insert(std::string &word)
-{
-    int nodeIdx = rootIdx;
-    for (auto &c: word)
-    {
-        if (nodes[nodeIdx].children.find(c) == nodes[nodeIdx].children.end())
-        {
-            nodes[nodeIdx].children[c] = nodes.size();
-            nodes.emplace_back();
-        }
-        nodeIdx = nodes[nodeIdx].children[c];
-    }
-    if (nodeIdx != rootIdx && nodes[nodeIdx].wordIdx < 0)
-    {
-        nodes[nodeIdx].wordIdx = vocab.size();
-        vocab.emplace_back(word);
-    } else
-    {
-        vocab[nodes[nodeIdx].wordIdx].count++;
-    }
-}
-
-void TrieDict::insert(std::string &&word)
-{
-    int nodeIdx = rootIdx;
-    for (auto &c: word)
-    {
-        if (nodes[nodeIdx].children.find(c) == nodes[nodeIdx].children.end())
-        {
-            nodes[nodeIdx].children[c] = nodes.size();;
-            nodes.emplace_back();
-        }
-        nodeIdx = nodes[nodeIdx].children[c];
-    }
-    if (nodeIdx != rootIdx && nodes[nodeIdx].wordIdx < 0)
-    {
-        nodes[nodeIdx].wordIdx = vocab.size();
-        vocab.emplace_back(word);
-    } else
-    {
-        vocab[nodes[nodeIdx].wordIdx].count++;
-    }
-}
-
-bool TrieDict::find(std::string &word)
-{
-    int nodeIdx = rootIdx;
-    for (auto &c: word)
-    {
-        if (nodes[nodeIdx].children.find(c) == nodes[nodeIdx].children.end())
-            return false;
-        nodeIdx = nodes[nodeIdx].children[c];
-    }
-    return nodes[nodeIdx].wordIdx >= 0;
-}
-
-
-TrieNode::TrieNode() : wordIdx(-1)
-{
-}
 
 Word::Word() : word(nullptr), count(0)
 {
